47d81f708f142ea19d69f35c040f6e5ea36d5579
Sanek182 <maxpower_90>
2023-05-10 16:22:01 +0300

    new changes to html-css-js files for page4

--- page4Script.js
+++ page4Script.js
@@ -1,32 +1,25 @@
-// Function to get user data from localStorage
-function getUsersFromLocalStorage() {
-    const storedUsers = localStorage.getItem('users');
-    return storedUsers ? JSON.parse(storedUsers) : [];
+function getUserFromLocalStorage() {
+    const storedUser = localStorage.getItem('user');
+    return storedUser ? JSON.parse(storedUser) : null;
 }
 
-// Function to add user data to the table
-function addUserToTable(user, index) {
-    const table = document.getElementById('registrationTable');
-    const tbody = table.querySelector('tbody');
+function setUserProfile(user) {
+    if (!user) return;
 
-    const row = document.createElement('tr');
-    row.innerHTML = `
-        <td>${index + 1}</td>
-        <td><img src="${user.image}" alt="${user.name}'s image" width="100"></td>
-        <td>${user.name} ${user.surname}</td>
-        <td>${user.email}</td>
-        <td>${user.birthDate}</td>
-        <td>${user.city}</td>
-        <td>${user.street}, ${user.streetNumber}</td>
-        <td><img src="${user.image}" alt="${user.name}'s image" width="100"></td>
-        <td>${user.hobbies}</td>
-    `;
+    document.getElementById('image-box').src = user.image;
+    document.getElementById('name').textContent = `${user.name} ${user.surname}`;
+    document.getElementById('email').textContent = `Email: ${user.email}`;
+    document.getElementById('birth-date').textContent = `Birth Date: ${user.birthDate}`;
+    document.getElementById('city-box').textContent = `City: ${user.city}`;
+    document.getElementById('street-box').textContent = `Street: ${user.street} ${user.streetNumber ? user.streetNumber : ''}`;
 
-    tbody.appendChild(row);
+    const hobbiesList = document.getElementById('hobbies-box');
+    user.hobbies.forEach(hobby => {
+        const listItem = document.createElement('li');
+        listItem.textContent = hobby;
+        hobbiesList.appendChild(listItem);
+    });
 }
 
-// Get user data from localStorage and populate the table
-const users = getUsersFromLocalStorage();
-users.forEach((user, index) => {
-    addUserToTable(user, index);
-});
+const user = getUserFromLocalStorage();
+setUserProfile(user);
