22d34a43a2c75befb797f6ed0f46a0c9453956af
Luke H. Walton <lukehwalton@icloud.com>
2023-05-11 04:52:29 -0700

    added phase2.js

--- phase3.js
+++ phase3.js
@@ -3,36 +3,36 @@ function goBack(){
 }
 
 function submitPhase3() {
-    const imageInput = document.getElementById("image-box");
-    const hobbyBoxes = document.getElementsByName('hobbies');
-    const imageURL = imageInput.value.trim();
-    const hobbies = [];
-    let isValid = true;
-  
-    // Validate image url
-    validateImageURL(imageURL, function(imgValid){
-      if (!imgValid){
-        isValid = false;
-        imageInput.classList.add("invalid");
-      } else {
-        imageInput.classList.remove('invalid')
-      }
-    });
+  const imageInput = document.getElementById("image-box");
+  const hobbyBoxes = document.getElementsByName('hobbies');
+  const imageURL = imageInput.value.trim();
+  const hobbies = [];
+  let isValid = true;
 
-    //get a list of hobbies from the checkbox
-    for (var i= 0; i < hobbyBoxes.length; i++){
-      if (hobbyBoxes[i].checked) hobbies.push(hobbyBoxes[i].value);
+  // Validate image url
+  validateImageURL(imageURL, function(imgValid){
+    if (!imgValid){
+      isValid = false;
+      imageInput.classList.add("invalid");
+    } else {
+      imageInput.classList.remove('invalid')
     }
-  
-    console.log(`isValid is ${isValid}`)
-    if (isValid) {
-      const oldUser = localStorage.getItem("user");
-      const user = new User(oldUser.name, oldUser.email, oldUser.birthDate, oldUser.city, oldUser.street, oldUser.number);
-      user.updatePhase3(image, hobbies.join(","));
-  
-      localStorage.setItem("user", JSON.stringify(user));
-  
-      //Move on to summary page
-      window.location.href = "phase4Page.html";
-    }
+  });
+
+  //get a list of hobbies from the checkbox
+  for (var i= 0; i < hobbyBoxes.length; i++){
+    if (hobbyBoxes[i].checked) hobbies.push(hobbyBoxes[i].value);
   }
+
+  console.log(`isValid is ${isValid}`)
+  if (isValid) {
+    const oldUser = localStorage.getItem("user");
+    const user = new User(oldUser.name, oldUser.email, oldUser.birthDate, oldUser.city, oldUser.street, oldUser.number);
+    user.updatePhase3(image, hobbies.join(","));
+
+    localStorage.setItem("user", JSON.stringify(user));
+
+    //Move on to summary page
+    window.location.href = "phase4Page.html";
+  }
+}
